3545221417f7eaaf9fa5fa5fbf9e248de2b805f2
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-02-03 18:39:02 +0200

    Initial project setup

---
+++ StructSamples/StructSamples/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StructSamples
+{
+    public struct Point
+    {
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public int this[string axis]
+        {
+            get
+            {
+                if (string.Equals(axis, "X", StringComparison.OrdinalIgnoreCase))
+                {
+                    return this.X;
+                }
+                else if (string.Equals(axis, "Y", StringComparison.OrdinalIgnoreCase))
+                {
+                    return this.Y;
+                }
+
+                return 0;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Point found at X={X}, Y={Y}");
+        }
+    }
+}
---
+++ StructSamples/StructSamples/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StructSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(100, 150);
+            Console.WriteLine($"X={p.X}, Y={p.Y}");
+            int x = p["X"];
+            int y = p["Y"];
+            Console.WriteLine($"X={x}, Y={y}");
+            p.Print();
+        }
+    }
+}
